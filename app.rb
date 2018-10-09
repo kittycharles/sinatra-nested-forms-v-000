1bf29630357e70426c95207f17956f8f0d39d08f
Richard Sanchez <richard.charles.s@gmail.com>
2018-10-09 17:11:23 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -12,7 +12,7 @@ module FormsLab
       erb :"/pirates/new"
     end
 
-    post '/show' do
+    post '/pirates' do
         @pirate = Pirate.new(params[:pirate])
 
         params[:pirate][:ships].each do |ship_details|
